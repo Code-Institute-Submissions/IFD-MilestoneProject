0c2525c90b9fabfa23c5a1775611750b3f03a3e9
Colman Leung <comacoma0218@hotmail.com>
2018-06-13 11:26:36 +0100

    UI element of poi v2 implemented (not functional yet).

--- assets/script/UI.js
+++ assets/script/UI.js
@@ -13,3 +13,29 @@ $(window).resize(function() {
 function resetToolbarUI() {
   $('.toolbar-button').removeClass('active');
 }
+
+var options = [];
+
+//Code taken from https://codepen.io/bseth99/pen/fboKH
+$( '.dropdown-menu a' ).on( 'click', function( event ) {
+
+   var $target = $( event.currentTarget ),
+       val = $target.attr( 'data-value' ),
+       $inp = $target.find( 'input' ),
+       idx;
+
+   if ( ( idx = options.indexOf( val ) ) > -1 ) {
+      options.splice( idx, 1 );
+      setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+   } else {
+      options.push( val );
+      setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+   }
+
+   $( event.target ).blur();
+
+   console.log( options );
+   return false;
+});
+
+//End of referenced code.
